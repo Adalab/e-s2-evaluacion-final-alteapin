c3410aff04e79e1cab8bb4f1edc267809bd56a39
alteapin <alteap18@gmail.com>
2018-12-17 20:28:43 +0100

    trying to change class

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -12,36 +12,51 @@ function showTvshow(event) {
     fetch(`https://api.tvmaze.com/search/shows?q=${inputEl.value}`)
         .then(response => response.json())
         .then(data => {
-console.log(data);
-          
+            console.log(data);
+
 let list = "";
-for (let i = 0; i < data.length; i++) {
-const nameShow = data[i].show.name;
-console.log(nameShow);
-
-list += `<li><h2>${nameShow}</h2></li>`;
+    for (let i = 0; i < data.length; i++) {
+        const nameShow = data[i].show.name;
+        console.log(nameShow);
 
 
-for (let i = 0; i < data.length; i++) {
-    const image = data[i].show.image;
-    // const showImage = image.original;
-    
-
-if (image === null) {
-    list += `<li><h2>${nameShow}</h2><img class="image-size" src="https://via.placeholder.com/210x295/cccccc/666666/?text=TV"></li>`
-} else {
- list += `<li><h2>${nameShow}</h2><img class="image-size" src=${image.original}></li>`;
-}
-
-    }
-    
-
-}
-
-
-listEl.innerHTML = list;
-
+    for (let i = 0; i < data.length; i++) {
+        const image = data[i].show.image;
+         if (image === null) {
+            list += `<li><h2>${nameShow}</h2><img class="image-size" src="https://via.placeholder.com/210x295/cccccc/666666/?text=TV"></li>` 
+        }  else {
+             list += `<li><h2>${nameShow}</h2><img class="image-size" src=${image.original}></li>`;}
+ }
+ }
+ listEl.innerHTML = list;
 });
 };
 
 buttonEl.addEventListener('click', showTvshow);
+
+//FAVOURITE 
+
+/**
+ * Esta es la función que ejecuta el navegador cuando la usuaria hace click.
+ * Y lo que hace es intercambiar la clase que se ocupa de ocultar la imagen
+ */
+const showTheTv = (e) => {
+    const item = e.currentTarget;
+    item.classList.toggle('color');
+  };
+  
+  /**
+   * Esta nueva función flecha me busca todas las noticias
+   * y les añade un listener para el evento 'click':
+   * llamará a la función `showTheImage()`
+   */
+  const addClickListener = () => {
+    const shows = listEl.querySelectorAll('.list');
+    for (const show of shows) {
+      show.addEventListener('click',showTheImage);
+    }
+  };
+  
+  // Llamo a mi función y le pado el array de noticias y la referencia a mi lista `.news`
+//   getWriteAndMarkMarsNews(api, shows);
+
