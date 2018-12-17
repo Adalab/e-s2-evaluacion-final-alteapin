327c4024aa4a16583400c22ff99ee58b516e739c
alteapin <alteap18@gmail.com>
2018-12-17 19:21:55 +0100

    showing image

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,54 +5,43 @@
 const inputEl = document.querySelector('.input');
 const buttonEl = document.querySelector('.button');
 const listEl = document.querySelector('.list');
+const imageEl = document.querySelector('.image');
 
 function showTvshow(event) {
     event.preventDefault();
-    fetch(`http://api.tvmaze.com/search/shows?q=${inputEl.value}`)
-    .then(response => response.json())
-    .then(data => {
-   console.log(data);
+    fetch(`https://api.tvmaze.com/search/shows?q=${inputEl.value}`)
+        .then(response => response.json())
+        .then(data => {
+console.log(data);
+          
+let list = "";
+for (let i = 0; i < data.length; i++) {
+const nameShow = data[i].show.name;
+console.log(nameShow);
 
+list += `<li><h2>${nameShow}</h2></li>`;
+
+
+for (let i = 0; i < data.length; i++) {
+    const image = data[i].show.image;
+    // const showImage = image.original;
+    
+
+if (image === null) {
+    list += `<li><h2>${nameShow}</h2><img src="https://via.placeholder.com/210x295/cccccc/666666/?text=TV"></li>`
+} else {
+ list += `<li><h2>${nameShow}</h2><img src=${image.original}></li>`;
+}
+
+    }
+    
+
+}
+
+
+listEl.innerHTML = list;
 
 });
-
 };
 
-
-
-
-
-
-
-
-
-
-
 buttonEl.addEventListener('click', showTvshow);
-
-// function searchCharacter(event) {
-//     event.preventDefault();
-//     fetch(`https://swapi.co/api/people/?search=${inputEl.value}`)
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data);
-//         const character = data.results;
-      
-//         let list ="";
-//         for(let i=0; i<character.length; i++) {
-//             const nameCharacter = character[i].name;
-//             const numberOfFilms = character[i].films.length;
-//             list += `<li>${nameCharacter} y el número de pelis ${numberOfFilms}</li>` 
-//         }
-
-// listEl.innerHTML = list;
-
-//     })
-   
-    
-// }
-
-
-
-
-// buttonEl.addEventListener('click', searchCharacter);
