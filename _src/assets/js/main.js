cf6e93f32d651485ca224c9352fa417bfb9d2ee2
alteapin <alteap18@gmail.com>
2018-12-18 18:39:00 +0100

    change Readme

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -15,20 +15,22 @@ function showData(event) {
         .then(data => {
             console.log(data);
 
+//GET NAMES & IMAGES
+
 let list = '';
     for (let i = 0; i < data.length; i++) {
         const nameShow = data[i].show.name;
         const imageShow = data[i].show.image;
-        let idShow = data[i].show.id;
+        const idShow = data[i].show.id;
         console.log(idShow);
 
     if (imageShow === null) {
-     list += `<li class="item__list">
+     list += `<li class="item__list" id="${idShow}">
             <h2>${nameShow}</h2>
             <img class="image__size" src="https://via.placeholder.com/210x295/FF9671/666666/?text=TV">
             </li>`; 
     } else {
-    list += `<li class="item__list">
+    list += `<li class="item__list" id="${idShow}">
              <h2>${nameShow}</h2>
              <img class="image__size" src=${imageShow.original}>
              </li>`;}
@@ -41,13 +43,23 @@ let list = '';
 
 buttonEl.addEventListener('click', showData);
 
-//ADD FAVOURITE
+//LOCAL STORAGE
 
+// localStorage.setItem('localData', JSON.stringify(idShow));
+
+
+
+
+//ADD FAVOURITE
 const addFavouriteShow = (event) => {
     const currentShow = event.currentTarget;
     currentShow.classList.toggle('add__favourite');
+//     const currentFavorite = document.querySelectorAll('add__favourite');
+//     console.log(currentFavorite);
+//     for (var i = 0; i < NodeList.values; i++) {
+//         localStorage.setItem('localData', JSON.stringify(currentFavorite));
+// };
 };
-
 
 
 const selectFavouriteShow = () => {
@@ -57,14 +69,12 @@ const selectFavouriteShow = () => {
     }
 };
 
+let savedLocalData = JSON.parse(localStorage.getItem('localData'));
 
 //LOCAL STORAGE
 
 
 
-idShow.push(idShow); // ¿Podría ser el item del for de favoritos?
-localStorage.setItem('localData', JSON.stringify(idShow));
-const savedLocalData = JSON.parse(localStorage.getItem('localData'));
-
-
+// savedLocalData.push(idShow); // 
 // Guardar el LocalStorage en la misma function que favoritos
+
