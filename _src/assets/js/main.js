00abfd3e89934769a6d8862fe59fa11a18bda6bd
alteapin <alteap18@gmail.com>
2018-12-19 15:29:00 +0100

    initial commit

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -18,20 +18,28 @@ function showData(event) {
 //GET NAMES & IMAGES
 
 let list = '';
+let genderlist ='';
     for (let i = 0; i < data.length; i++) {
         const nameShow = data[i].show.name;
+        const genderShow = data[i].show.genres;
         const imageShow = data[i].show.image;
         const idShow = data[i].show.id;
         console.log(idShow);
 
+for ( const gender of genderShow) {
+ genderlist += `<li>${gender}</li>`;
+}
+
     if (imageShow === null) {
      list += `<li class="item__list" id="${idShow}">
             <h2>${nameShow}</h2>
+            <ul>${genderlist}</ul>
             <img class="image__size" src="https://via.placeholder.com/210x295/FF9671/666666/?text=TV">
             </li>`; 
     } else {
     list += `<li class="item__list" id="${idShow}">
              <h2>${nameShow}</h2>
+             <ul>${genderlist}</ul>
              <img class="image__size" src=${imageShow.original}>
              </li>`;}
  }
@@ -50,6 +58,7 @@ const addFavouriteShow = (event) => {
     const currentShow = event.currentTarget;
     currentShow.classList.toggle('add__favourite');
 
+
     const idItem = document.querySelectorAll('item__list');
     // const input = document.querySelectorAll('add__favourite');
 
@@ -60,12 +69,24 @@ const addFavouriteShow = (event) => {
 };
 
 
+const showName = (event) => {
+    const currentShowname = event.currentTarget;
+    console.log(currentShowname);
+    const nameCurrent = currentShowname.querySelector('h2');
+    console.log(nameCurrent.innerHTML);
+}
+
+
 const selectFavouriteShow = () => {
     const itemShow = document.querySelectorAll('.item__list');
-    for ( const item of itemShow) {
-        item.addEventListener('click', addFavouriteShow);
+    for (const item of itemShow){
+        // console.log(itemShow);
+        item.addEventListener('click', showName);
     }
+  
 };
+
+
 
 
 
